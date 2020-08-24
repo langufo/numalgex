f5a69492bdee8737f6b98c4b3d4b7095e5a7e030
Fabio Luongo <fabio.luongo@edu.unito.it>
2020-08-25 00:21:59 +0200

    implemented functionality to control the order of update

--- GSeidelSolver.cpp
+++ GSeidelSolver.cpp
@@ -6,54 +6,60 @@
 #include "Matrix.hpp"
 
 GSeidelSolver::GSeidelSolver(double (*rhs)(double, double),
-                             BndryLayout neumLayout,
-                             double x0,
-                             double y0,
-                             double h,
-                             int nX,
-                             int nY,
-                             const double* bottom,
-                             const double* top,
-                             const double* left,
-                             const double* right)
+                             BndryLayout neumLayout, double x0, double y0,
+                             double h, int nX, int nY, const double *bottom,
+                             const double *top, const double *left,
+                             const double *right)
   : PDESolver(rhs, neumLayout, x0, y0, h, nX, nY, bottom, top, left, right)
 {}
 
 double
-GSeidelSolver::line_update(int i,
-                           int jFirst,
-                           int jLast,
-                           BndryLayout neum,
-                           const double* bottom,
-                           const double* top,
-                           const double* left,
-                           const double* right)
+GSeidelSolver::line_update(int i, int jInf, int jSup, BndryLayout neum,
+                           const double *bottom, const double *top,
+                           const double *left, const double *right)
 {
   double sum = 0;
 
-  for (int j = jFirst; j <= jLast; ++j) {
-    int k = j - jFirst;
-    double a = ms[i][j];
-    ms[i][j] = next_value(
-      neum, x[i], y[j], bottom[k], top[k], left[k], right[k], mr[i][j]);
-    sum += fabs(ms[i][j] - a);
+  int step = revY ? -1 : 1;
+  int jFirst = revY ? jSup : jInf;
+  int jLast = revY ? jInf : jSup;
+
+  for (int j = jFirst; j <= jLast; j += step) {
+    double old = ms[i][j];
+
+    ms[i][j] =
+      next_value(neum, x[i], y[j], *bottom, *top, *left, *right, mr[i][j]);
+
+    sum += fabs(ms[i][j] - old);
+
+    top += step;
+    bottom += step;
+    left += step;
+    right += step;
   }
 
   return sum;
 }
 
 double
-GSeidelSolver::iter(double* s, bool resAsErr)
+GSeidelSolver::iter(double *s, bool resAsErr)
 {
   ms.set_first_elem(s);
 
   double eps = 0;
 
-  for (int p = 0; p < nRegX; ++p) {
-    for (int q = 0; q < nRegY; ++q) {
-      int iFirst = limX[p][0];
-      int jFirst = limY[q][0];
-      int jLast = limY[q][1];
+  int hStep = revX ? -nY : nY;
+  int iStep = revX ? -1 : 1;
+
+  for (int c = 0; c < nRegX; ++c) {
+    int p = revX ? nRegX - 1 - c : c;
+    for (int d = 0; d < nRegY; ++d) {
+      int q = revY ? nRegY - 1 - d : d;
+
+      int iFirst = revX ? limX[p][1] : limX[p][0];
+      int iLast = revX ? limX[p][0] : limX[p][1];
+      int jInf = limY[q][0];
+      int jSup = limY[q][1];
 
       const double *b, *t, *l, *r;
       int bStep, tStep;
@@ -62,35 +68,42 @@ GSeidelSolver::iter(double* s, bool resAsErr)
         b = bottom + iFirst;
         bStep = 1;
       } else {
-        b = ms[iFirst] + jFirst - 1;
+        b = ms[iFirst] + jInf - 1;
         bStep = nY;
       }
       if (bndryY[q] & TOPBNDRY) {
         t = top + iFirst;
         tStep = 1;
       } else {
-        t = ms[iFirst] + jFirst + 1;
+        t = ms[iFirst] + jInf + 1;
         tStep = nY;
       }
       if (bndryX[p] & LEFTBNDRY) {
-        l = left + jFirst;
+        l = left + jInf;
       } else {
-        l = ms[iFirst - 1] + jFirst;
+        l = ms[iFirst - 1] + jInf;
       }
       if (bndryX[p] & RIGHTBNDRY) {
-        r = right + jFirst;
+        r = right + jInf;
       } else {
-        r = ms[iFirst + 1] + jFirst;
+        r = ms[iFirst + 1] + jInf;
       }
 
-      for (int i = limX[p][0]; i <= limX[p][1]; ++i) {
-        eps += line_update(
-          i, jFirst, jLast, neumLayout & (bndryX[p] | bndryY[q]), b, t, l, r);
+      if (revY) {
+        bStep = -bStep;
+        tStep = -tStep;
+      }
+
+      for (int i = iFirst; i != iLast; i += iStep) {
+        eps += line_update(i, jInf, jSup, neumLayout & (bndryX[p] | bndryY[q]),
+                           b, t, l, r);
         b += bStep;
         t += tStep;
-        l += nY;
-        r += nY;
+        l += hStep;
+        r += hStep;
       }
+      eps += line_update(iLast, jInf, jSup,
+                         neumLayout & (bndryX[p] | bndryY[q]), b, t, l, r);
     }
   }
   eps *= h * h;
@@ -100,4 +113,11 @@ GSeidelSolver::iter(double* s, bool resAsErr)
   }
 
   return eps;
+}
+
+void
+GSeidelSolver::rev_solv_direc(bool revX, bool revY)
+{
+  GSeidelSolver::revX = revX;
+  GSeidelSolver::revY = revY;
 }
--- GSeidelSolver.hpp
+++ GSeidelSolver.hpp
@@ -6,28 +6,20 @@
 class GSeidelSolver : public virtual PDESolver
 {
 public:
-  GSeidelSolver(double (*rhs)(double, double),
-                BndryLayout neumLayout,
-                double x0,
-                double y0,
-                double h,
-                int nX,
-                int nY,
-                const double* bottom,
-                const double* top,
-                const double* left,
-                const double* right);
-  double iter(double* sol, bool resAsErr) final;
+  GSeidelSolver(double (*rhs)(double, double), BndryLayout neumLayout,
+                double x0, double y0, double h, int nX, int nY,
+                const double *bottom, const double *top, const double *left,
+                const double *right);
+  double iter(double *sol, bool resAsErr) final override;
+  void rev_solv_direc(bool revX, bool revY);
 
 private:
-  double line_update(int i,
-                     int jFirst,
-                     int jLast,
-                     BndryLayout neum,
-                     const double* bottom,
-                     const double* top,
-                     const double* left,
-                     const double* right);
+  double line_update(int i, int jInf, int jSup, BndryLayout neum,
+                     const double *bottom, const double *top,
+                     const double *left, const double *right);
+
+  bool revX;
+  bool revY;
 };
 
 #endif
