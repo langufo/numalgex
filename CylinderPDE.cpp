75213039a24ab0b33909cab2820655a988c02ed4
Fabio Luongo <fabio.luongo@edu.unito.it>
2020-11-30 02:18:06 +0100

    fixed some errors in the expression for the residual

--- CylinderPDE.cpp
+++ CylinderPDE.cpp
@@ -3,8 +3,6 @@
 #include "BndryProp.hpp"
 #include "Real.hpp"
 
-#define BETTER
-
 Real
 CylinderPDE::next_value(Real r,
                         Real z,
@@ -32,8 +30,8 @@ CylinderPDE::next_value(Real r,
       denom += (r - h) * 2;
       num += right * h * (2 * r + h) + left * (r - h) * 2;
       break;
-    default: // LEFTBNDRY | RIGHTBNDRY
-      num += (right * (r + h) - left * (r - h)) * h * (static_cast<Real>(3)/2);
+    default: // case LEFTBNDRY | RIGHTBNDRY
+      num += (right * (r + h) - left * (r - h)) * h * 3 / 2;
   }
 
   switch (neum & (BOTTOMBNDRY | TOPBNDRY)) {
@@ -49,8 +47,8 @@ CylinderPDE::next_value(Real r,
       denom += 2 * r;
       num += (bottom + top * h) * 2 * r;
       break;
-    default: // BOTTOMBNDRY | TOPBNDRY
-      num += (top - bottom) * h * (static_cast<Real>(3)/2) * r;
+    default: // case BOTTOMBNDRY | TOPBNDRY
+      num += (top - bottom) * h * 3 / 2 * r;
   }
 
   return (num - rhs * h * h * 3 * r) / denom;
@@ -70,25 +68,6 @@ CylinderPDE::residual(Real r,
 {
   Real lhs = 0;
 
-#ifndef BETTER
-  switch (neum & (LEFTBNDRY | RIGHTBNDRY)) {
-    case 0:
-      lhs += (right - left) * h / 2 / r + right + left - 2 * middle;
-      break;
-    case LEFTBNDRY:
-      lhs += (((right - middle) * 2 + left * h) * h / r +
-              (right - left * h - middle) * 2) /
-             3;
-      break;
-    case RIGHTBNDRY:
-      lhs += ((right * h + (middle - left) * 2) * h / r +
-              (right * h + left - middle) * 2) /
-             3;
-      break;
-    default: // case LEFTBNDRY | RIGHTBNDRY:
-      lhs += ((right + left) * h / r + right - left) * h / 2;
-  }
-#else
   switch (neum & (LEFTBNDRY | RIGHTBNDRY)) {
     case 0:
       lhs += right * (1 + h / r / 2) + left * (1 - h / r / 2) - 2 * middle;
@@ -102,17 +81,16 @@ CylinderPDE::residual(Real r,
     default: // case LEFTBNDRY | RIGHTBNDRY:
       lhs += (right * (1 + h / r) - left * (1 - h / r)) * h / 2;
   }
-#endif
 
   switch (neum & (BOTTOMBNDRY | TOPBNDRY)) {
     case 0:
       lhs += bottom + top - middle * 2;
       break;
     case BOTTOMBNDRY:
-      lhs += top + bottom * h - middle;
+      lhs += (top - bottom * h - middle) * 2 / 3;
       break;
     case TOPBNDRY:
-      lhs += bottom + top * h - middle;
+      lhs += (bottom + top * h - middle) * 2 / 3;
       break;
     default: // case BOTTOMBNDRY | TOPBNDRY:
       lhs += (top - bottom) * h / 2;
